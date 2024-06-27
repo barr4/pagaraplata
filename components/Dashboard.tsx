723150adf3a9c9b9e82b351346a57139ee184067
barra <joacodiana1998@gmail.com>
2024-06-26 23:39:23 -0300

    feat: add dashboard component to home route

---
+++ components/Dashboard.tsx
@@ -0,0 +1,9 @@
+
+
+export default function HomePage () {
+    return (
+        <section className='flex flex-col items-center bg-white text-black rounded-lg p-8'>
+            <h1 className='text-5xl font-bold'>Pagaraplata</h1>
+        </section>
+    )
+}
